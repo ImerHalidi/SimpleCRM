8c4f0db84cdc14d6995ef67de9fc39c5b0cad2e3
Imer Halidi <I.halidi220039@unite.edu.mk>
2026-05-03 16:46:49 +0200

    AbstractEntity and his dependency in pom.xml

--- src/main/java/org/example/common/AbstractEntity.java
+++ src/main/java/org/example/common/AbstractEntity.java
@@ -1,4 +1,39 @@
 package org.example.common;
+import com.google.gson.annotations.SerializedName;
 
-public class AbstractEntity {
+public abstract class AbstractEntity {
+    @SerializedName("id")
+    private Long id;
+
+    @SerializedName("created_at")
+    private String createdAt;
+
+    @SerializedName("updated_at")
+    private String updatedAt;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id=id;
+    }
+
+    public String getCreatedAt(){
+        return createdAt;
+    }
+    public void setCreatedAt(String createdAt){
+        this.createdAt=createdAt;
+    }
+
+    public String getUpdatedAt(){
+        return updatedAt;
+    }
+    public void setUpdatedAt(String updatedAt){
+        this.updatedAt=updatedAt;
+    }
+
+    public abstract String validate();
+
+
 }
