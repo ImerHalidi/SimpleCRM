e4cee59a3381cc4941a4d453078184858f805834
Imer Halidi <I.halidi220039@unite.edu.mk>
2026-05-03 19:09:39 +0200

    abstractService and DatabaseConnection.java added and modified

--- src/main/java/org/example/common/DatabaseConnection.java
+++ src/main/java/org/example/common/DatabaseConnection.java
@@ -1,4 +1,38 @@
 package org.example.common;
 
+import javax.xml.transform.Result;
+import java.sql.*;
+
 public class DatabaseConnection {
+    private static final String Url = "jdbc:mysql://http://localhost/phpmyadmin/index.php?route=/database/structure&db=simple_crm";
+    private static final String Username = "root";
+    private static final String Password = "";
+
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(Url,Username,Password);
+    }
+
+    public static void close(Connection con , PreparedStatement ps) {
+        try{
+            if(ps!= null )ps.close();
+        } catch (SQLException e) {}
+
+        try {
+            if(con!=null)con.close();
+        } catch (SQLException e) {
+
+        }
+
+    }
+
+    public static void close(Connection con, PreparedStatement ps, ResultSet rs){
+        try {
+            if (rs != null) rs.close();
+        } catch (SQLException e) {
+
+        }
+        close(con, ps);
+        }
+
+
 }
--- src/main/java/org/example/common/abstractService.java
+++ src/main/java/org/example/common/abstractService.java
@@ -1,4 +1,28 @@
 package org.example.common;
 
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class abstractService {
+
+    protected Connection getConnection() throws SQLException {
+        return DatabaseConnection.getConnection();
+    }
+    protected void close(Connection con, PreparedStatement ps){
+        DatabaseConnection.close(con,ps);
+    }
+
+    protected void close(Connection con, PreparedStatement ps, ResultSet rs){
+        DatabaseConnection.close(con,ps,rs);
+    }
+
+    protected  RuntimeException handleException(Exception e){
+        e.printStackTrace();
+        return new RuntimeException(e.getMessage());
+    }
+
+
 }
