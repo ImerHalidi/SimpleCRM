3f0f22d6e28831902009f20e3c715382b8551355
Imer Halidi <I.halidi220039@unite.edu.mk>
2026-05-06 20:00:08 +0200

    ContactPerson and Customer classes added

--- src/main/java/org/example/domain/ContactPerson.java
+++ src/main/java/org/example/domain/ContactPerson.java
@@ -1,4 +1,76 @@
 package org.example.domain;
+import com.google.gson.annotations.SerializedName;
+import org.example.common.AbstractEntity;
 
-public class ContactPerson {
+public class ContactPerson extends AbstractEntity {
+    @SerializedName("customerId")
+    private Long customerId ;
+
+    @SerializedName("fullName")
+    private String fullName;
+
+    @SerializedName("email")
+    private String email;
+
+    @SerializedName("phone")
+    private String phone;
+
+    @SerializedName("position")
+    private String position;
+
+    public String validate(){
+        if(customerId==null ){
+            return "CustomerId Required";
+        }
+        if(fullName==null||fullName.trim().isEmpty()){
+            return "FullName Is Required";
+        }
+        if(email==null||email.trim().isEmpty()){
+            return "Email Is Required";
+        }
+        if(!email.contains("@")){
+            return "Email must have @ ";
+        }
+        if(position==null||position.trim().isEmpty()){
+            return "Position Is Required ";
+        }
+        return null;
+
+    }
+
+    public Long getCustomerId(){
+        return customerId;
+    }
+    public void setCustomerId(Long customerId){
+        this.customerId=customerId;
+    }
+
+    public String getFullName(){
+        return fullName;
+    }
+    public void setFullName(String fullName){
+        this.fullName=fullName;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
+    public void setPhone(String phone){
+        this.phone=phone;
+    }
+
+    public String getPosition(){
+        return position;
+    }
+    public void setPosition(String position){
+        this.position=position;
+    }
+
 }
--- src/main/java/org/example/domain/Customer.java
+++ src/main/java/org/example/domain/Customer.java
@@ -1,4 +1,73 @@
 package org.example.domain;
+import com.google.gson.annotations.SerializedName;
+import org.example.common.AbstractEntity;
 
-public class Customer {
+public class Customer extends AbstractEntity {
+    @SerializedName ("name")
+        private String name;
+    @SerializedName ("industry")
+    private  String industry;
+
+    @SerializedName ("email")
+    private String email;
+
+    @SerializedName ("phone")
+    private String phone;
+
+    @SerializedName ("status")
+    private String status;
+
+
+    @Override
+    public String validate(){
+        if(name==null || name.trim().isEmpty()){
+            return "Name is Empty";
+        }
+        if(email==null ||email.trim().isEmpty()){
+            return "Email is Empty";
+        }
+        if(!email.contains("@")){
+            return "Email need to content @";
+        }
+        if(status==null ||status.trim().isEmpty()){
+            return "Status is empty";
+        }
+        return null;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getIndustry(){
+        return industry;
+    }
+    public void setIndustry(String industry){
+        this.industry=industry;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
+    public void setPhone(String phone){
+        this.phone=phone;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+    public void setStatus(String status){
+        this.status=status;
+    }
+
 }
